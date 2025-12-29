18cd9307860bb7ecfe093eadd2869dd8691a8b2c
Devsingh0123 <devmns20@gmail.com>
2025-12-29 15:14:27 +0530

    fix some links in project section

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import AirbnbLogo from "../../assets/AirbnbLogo.png";
-const ProjectCard = ({ title, main,logo }) => {
+const ProjectCard = ({ title, main,logo,demoLink,GithubLink}) => {
   return (
     <div className="p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] rounded-lg shadow-xl shadow-slate-900 ">
       <img className="p-4 w-[100%] h-[25%] " src={logo} alt="" />
@@ -9,11 +9,11 @@ const ProjectCard = ({ title, main,logo }) => {
       </h3>
       <p className="px-4 text-sm md:text-md leading-tight py-2">{main}</p>
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
-        <a href="https://airbnb-clone-1-nqmg.onrender.com/">
+        <a href={demoLink}>
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#5dade243]">
           Demo
         </button></a>
-        <a href="https://github.com/Devsingh0123/Airbnb-clone">
+        <a href={GithubLink}>
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#5dade243]">
           Source Code
         </button></a>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 import AirbnbLogo from "../../assets/AirbnbLogo.png";
-import YouTubeLogo from "../../assets/YouTubeLogo.png"
+import YouTubeLogo from "../../assets/YouTubeLogo.png";
 
 const Projects = () => {
   return (
     <div id="Projects" className="p-12  text-white md:mx-20 ">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
 
-      <div className="py-12 
+      <div
+        className="py-12 
   px-4 sm:px-8 md:px-12 lg:px-20
   flex flex-wrap gap-6
   justify-center md:justify-start
   items-stretch
-  shadow-xl bg-opacity-30 rounded-lg">
+  shadow-xl bg-opacity-30 rounded-lg"
+      >
         <ProjectCard
           title="Airbnb-Clone"
           logo={AirbnbLogo}
           main="Developed a full-stack Airbnb-like web application enabling users to list properties, 
 browse listings, and manage bookings with secure authentication and role-based access. "
+          demoLink="https://airbnb-clone-1-nqmg.onrender.com/"
+          GithubLink="https://github.com/Devsingh0123/Airbnb-clone"
         />
         <ProjectCard
           title="Youtue Clone"
           logo={YouTubeLogo}
           main="Developed a full-stack Airbnb-like web application enabling users to list properties, 
 browse listings, and manage bookings with secure authentication and role-based access. "
+          demoLink="https://you-tube-clone-gilt-nine.vercel.app/"
+          GithubLink="https://github.com/Devsingh0123/YouTube-Clone"
         />
         <ProjectCard
           title="Netflix Clone"
