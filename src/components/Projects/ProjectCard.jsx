f5353fe7354c78f95ed6a1d792f10067bb8cb225
Devsingh0123 <devmns20@gmail.com>
2025-12-29 15:18:48 +0530

    fix anchor tag

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -9,11 +9,13 @@ const ProjectCard = ({ title, main,logo,demoLink,GithubLink}) => {
       </h3>
       <p className="px-4 text-sm md:text-md leading-tight py-2">{main}</p>
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
-        <a href={demoLink}>
+        <a href={demoLink}
+        target="_blank">
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#5dade243]">
           Demo
         </button></a>
-        <a href={GithubLink}>
+        <a href={GithubLink}
+        target="_blank">
         <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#5dade243]">
           Source Code
         </button></a>
